5a8dd6f1a7089295aa7fc26c3f9ba72edb4f511c
vileoleone <vileoleone@gmail.com>
2023-03-07 20:53:29 -0400

    feat: layout estático da página de hoteis

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,3 +1,33 @@
+import styled from 'styled-components';
+import HotelBox from './HotelBox';
+
 export default function Hotel() {
-  return 'Hotel: Em breve!';
+  return(
+    <>
+      <H1>Escolha de hotel e quarto</H1>
+      <H2>Primeiro, escolha seu hotel</H2>
+      <HotelBox></HotelBox>
+    </>
+  );
 }
+
+const H1 = styled.h1`
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 400;
+font-size: 34px;
+line-height: 40px;
+color: #000000;
+margin-bottom: 35px;
+`;
+
+const H2 = styled.h2`
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 400;
+font-size: 20px;
+line-height: 23px;
+color: #8E8E8E;
+margin-bottom: 18px;
+`;
+
