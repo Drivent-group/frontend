be909286230050e204fe6c0f5b4fa39373aad87d
vileoleone <vileoleone@gmail.com>
2023-03-18 18:33:41 -0400

    feat: funções de rotas de atividades

--- src/services/activitiesApi.js
+++ src/services/activitiesApi.js
@@ -12,3 +12,34 @@ export async function getActivitiesByDayId(token, dayId) {
     return null;
   }
 }
+
+export async function postSeat(token, activityId) {
+  const body = {
+    'activityId': activityId
+  };
+
+  await api.post('/activities/subscription', body, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return  window.location.reload();
+}
+
+export async function getAvailableSeats(token, activityId, dayId) {
+  const body = {
+    'activityId': activityId,
+    'dayId': dayId
+  };
+  try {
+    const response = await api.get('/activities/seats/', body, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (errors) {
+    return null;
+  }
+}
